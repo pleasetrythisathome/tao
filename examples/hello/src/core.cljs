a0c1bf85183cb7684a64ae7c819ffa95f5c06b99
Dylan Butman <dbutman@nyu.edu>
2014-04-11 11:05:26 -0400

    working example

--- examples/hello/src/core.cljs
+++ examples/hello/src/core.cljs
@@ -1,13 +1,64 @@
 (ns examples.hello.core
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [tao.core :refer [deftao]])
   (:require [tao.core :as tao :include-macros true]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [sablono.core :as html :refer-macros [html]]
+            [tao.utils :refer [deep-merge-with]]
+            [cljs.core.async :refer [<! chan]]))
 
-(def app-state (atom {}))
+(enable-console-print!)
 
-(defn widget [data]
-  (om/component
-    (dom/div nil "Hello world!")))
+(def app-state (atom {:active nil
+                      :sections [{:id :admin
+                                  :title "Admin"}
+                                 {:id :main
+                                  :title "Main"}
+                                 {:id :other
+                                  :title "Other"}]}))
 
-(om/root widget app-state
-  {:target (.getElementById js/document "app")})
+(defn button
+  [{{:keys [id title]} :data {:keys [active] :as app} :app} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (html
+       [:div {:class "button"
+              :on-click #(om/update! app :active id)}
+        (if (= active id)
+          (str title "<")
+          title)]))))
+
+(defn app-view
+  [{:keys [active sections] :as app} owner]
+  (reify
+    om/IWillMount
+    (will-mount [this]
+      (go
+       (while true
+         (let [[route state] (<! (om/get-shared owner :navigation))]
+           (om/transact! app [] #(deep-merge-with merge % state) :silent)))))
+    om/IRender
+    (render [_]
+      (html
+       [:div {:class "app"}
+        [:div
+         (map #(om/build button {:data % :app app} {:react-key (:id %)}) sections)]]))))
+
+
+(def nav-chan (chan))
+
+(deftao section "/:active"
+  {:chan nav-chan
+   :params {:active {:path []
+                     :->state keyword
+                     :->route name}}})
+
+;; you should really use push-state and set up
+;; your backend to forward all routes to the front end
+(tao/init-history {:push-state false})
+
+(om/root app-view app-state
+         {:target (.getElementById js/document "app")
+          :shared {:navigation nav-chan}
+          :tx-listen tao/update-history})
--- project.clj
+++ project.clj
@@ -1,11 +1,4 @@
 (defproject tao "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
-
-(defproject tao "0.1.0-SNAPSHOT"
   :description "Two way data binding for browser history"
   :url "http://github.com/pleasetrythisathome/tao"
   :license {:name "Eclipse"
